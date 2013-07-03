46dfc93f81e12e4a72885fca6103a8f9bd80aba4
Jason Garner <jason.m.garner@gmail.com>
2013-07-03 11:33:21 -0500

    Top bar structure updated

--- header.php
+++ header.php
@@ -22,37 +22,24 @@
 <div id="page" class="hfeed site">
 	<?php do_action( 'before' ); ?>
 	<header id="masthead" class="site-header" role="banner">
-		<!-- <div class="site-branding large-3 columns">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-		</div> -->
-
-<!-- 		<nav id="site-navigation" class="navigation-main large-9 columns" role="navigation">
-			<h1 class="menu-toggle"><?php _e( 'Menu', 'wp_lamar' ); ?></h1>
-			<div class="screen-reader-text skip-link"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'wp_lamar' ); ?>"><?php _e( 'Skip to content', 'wp_lamar' ); ?></a></div>
-
-			<?php
-			if ( has_nav_menu( 'primary' ) ) {
-				$args = array(
-					'theme_location' => 'primary',
-					'container' => '',
-					'items_wrap' => '<ul class="right"><li>%3$s</li></ul>',
-					'echo' => true,
-					'walker' => new top_bar_walker
-					);
-				wp_nav_menu( $args );
-			} else {
-				wp_nav_menu();
-			}
-			?> -->
-		</nav><!-- #site-navigation -->
-		<div class="top-bar-container contain-to-grid">
+		<?php
+		/**
+		* http://foundation.zurb.com/docs/components/top-bar.html
+		*
+		* Make fixed: class="top-bar-container fixed"
+		* Contain to grid: class="top-bar-container contain-to-grid"
+		*
+		* Remove "Menu" to just show the menu icon
+		*/
+		?>
+		<div class="top-bar-container">
 			<nav id="site-navigation" class="top-bar navigation-main" role="navigation">
 				<ul class="title-area site-branding">
 					<li class="name">
 						<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 						<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 					</li>
+					<li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
 				</ul>
 				<section class="top-bar-section">
 					<h1 class="menu-toggle"><?php _e( 'Menu', 'wp_lamar' ); ?></h1>
@@ -63,7 +50,7 @@
 						$args = array(
 							'theme_location' => 'primary',
 							'container' => '',
-							'items_wrap' => '<ul class="inline-list right"><li>%3$s</li></ul>',
+							'items_wrap' => '<ul class="right"><li>%3$s</li></ul>',
 							'echo' => true,
 							'walker' => new top_bar_walker
 							);
